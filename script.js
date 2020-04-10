44bbea871b90acd1a549283a73f12b3ae0e39380
biachinicecilia <bianchini.cecilia@gmail.com>
2020-04-10 10:30:27 +0100

    try

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let innerCircleRad, outerCircleRad, ww, wh;
 
 window.addEventListener('resize', getElementSizes);
 function getElementSizes() {
-  let innerCircle = document.querySelector('.inner-circle');
-  let outerCircle = document.querySelector('.outer-circle');
+  let innerCircle = document.querySelector('#inner-circle');
+  let outerCircle = document.querySelector('#outer-circle');
 
   innerCircleRad = innerCircle.clientHeight / 2;
   outerCircleRad = outerCircle.clientHeight / 2;
@@ -12,14 +12,19 @@ function getElementSizes() {
 }
 getElementSizes();
 
-if (screen.width < 600) {
-
-}
-
 document.addEventListener('mousemove', handler);
+document.querySelector('#inner-circle').addEventListener('touchstart', function() {
+  document.addEventListener('touchmove', handler);
+});
+document.addEventListener('touchend', function() {
+  document.removeEventListener('touchmove', handler);
+});
+
 function handler(e) {
   e = e || window.event;
-
+  if('touches' in e) {
+    e = e.touches[0];
+  }
   let mousex = e.pageX;
   let mousey = e.pageY;
 
@@ -34,5 +39,33 @@ function handler(e) {
     yoffset *= scaleDown;
   }
   //document.querySelector('#inner-circle').style.transform = 'translate(' + (xoffset - innerCircleRad) + 'px, ' + (yoffset - innerCircleRad) + 'px)';
-  gsap.to(".inner-circle", {duration: 0.25, x: (xoffset), y: (yoffset)});
+  gsap.to('#inner-circle', {duration: 0.25, x: (xoffset), y: (yoffset)});
+}
+
+/* MODAL */
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
